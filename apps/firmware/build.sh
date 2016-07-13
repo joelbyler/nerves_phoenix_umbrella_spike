d4049d951b312de22180de8d8af0a220aaaf3161
Joel Byler <jbyler@covermymeds.com>
2016-07-12 22:57:23 -0400

    Latest iteration, dnsmasq starts up now

--- apps/firmware/build.sh
+++ apps/firmware/build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 export NERVES_SYSTEM=~/Workspaces/nerves_systems/nerves_system_rpi3
+echo $NERVES_SYSTEM
 
 mix compile
 mix firmware
 mix firmware.burn
-
-diskutil unmountDisk disk2
--- apps/firmware/lib/firmware.ex
+++ apps/firmware/lib/firmware.ex
@@ -22,17 +22,15 @@ defmodule Firmware do
 
     migrate
 
-    IO.puts "Running custom initialization script"
-    {finit_output, return_val} = System.cmd("finit", ["start"])
-    IO.puts "initialization result: #{finit_output}; #{return_val}"
+    # IO.puts "Running custom initialization script"
+    # {finit_output, return_val} = System.cmd("finit", ["start"])
+    # IO.puts "initialization result: #{finit_output}; #{return_val}"
 
     # IO.puts "Trying again with porcelain"
     # %Result{out: porcelain_output, status: porcelain_status} = Porcelain.shell("finit start")
     # IO.puts "porcelain result: #{porcelain_output}; #{porcelain_status}"
 
-    # IO.puts "Trying to start hostapd directly"
-    # {hostapd_output, hostapd_return_val} = System.cmd("hostapd", ["-d", "/etc/hostapd/hostapd.conf"])
-    # IO.puts "initialization result: #{hostapd_output}; #{hostapd_return_val}"
+    start_network
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
@@ -49,6 +47,16 @@ defmodule Firmware do
     path = Application.app_dir(:user_interface, "priv/repo/migrations")
 
     Ecto.Migrator.run(UserInterface.Repo, path, :up, all: true)
+  end
+
+  defp start_network do
+    IO.puts "Starting dnsmasq"
+    {dnsmasq_output, dnsmasq_return_val} = System.cmd("dnsmasq", [])
+    IO.puts "dnsmasq result: #{dnsmasq_output}; #{dnsmasq_return_val}"
+
+    IO.puts "Starting hostapd"
+    {hostapd_output, hostapd_return_val} = System.cmd("hostapd", ["-d", "/etc/hostapd/hostapd.conf"])
+    IO.puts "hostapd result: #{hostapd_output}; #{hostapd_return_val}"
   end
 
 end
