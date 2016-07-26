e8c304da0a2ea54030cbc06a8a99f3d15fe7a643
Joel Byler <jbyler@covermymeds.com>
2016-07-25 20:35:11 -0400

    Behold the power of pattern matching

--- apps/user_interface/web/helpers/user_helper.ex
+++ apps/user_interface/web/helpers/user_helper.ex
@@ -4,8 +4,22 @@ defmodule UserInterface.UserHelper do
   def user_ip_address(conn), do: Enum.join(Tuple.to_list(conn.remote_ip),".")
 
   def user_mac_address(conn) do
-    {arp_result, arp_status} = System.cmd("arp", ["-a", user_ip_address(conn)])
-    # TODO: throw exception unless arp_status == 0
-    List.first(Regex.run(~r/([0-9a-fA-F]{2}[:-]){5}[0-9a-fA-F]{2}/, arp_result))
+    user_ip_address(conn) |> fetch_arp |> default_arp |> parse_arp_response
+  end
+
+  defp fetch_arp(ip) do
+    {arp_result, arp_status} = System.cmd("arp", ["-a", ip])
+  end
+
+  defp default_arp({_, 1}) do
+    {arp_result, arp_status} = System.cmd("arp", ["-a"])
+  end
+
+  defp parse_arp_response({arp_response, 0}) do
+    List.first(Regex.run(~r/([0-9a-fA-F]{2}[:-]){5}[0-9a-fA-F]{2}/, arp_response || []))
+  end
+
+  defp parse_arp_response({_, _}) do
+    ""
   end
 end
